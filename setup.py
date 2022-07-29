c0ca1e562ec8d03c1c729452c4358923b5701460
laar <3553751921101@ingenieria.usac.edu.gt>
2022-07-28 18:56:33 -0600

    sin setup.cfg

--- setup.py
+++ setup.py
@@ -1,4 +1,16 @@
 from setuptools import setup
-
-if __name__ == "__main__":
-    setup()
+setup(
+    name='app-name',
+    version='1.0',
+    author='Bejamin Frakline',
+    description='A brief synopsis of the project',
+    long_description='A much longer explanation of the project and helpful resources',
+    url='https://github.com/BenjaminFranline',
+    keywords='development, setup, setuptools',
+    python_requires='>=3.7',
+    install_requires=[
+        'fredapi',
+        'xlrd',
+        'prettytable'
+    ]
+)
