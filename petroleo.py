a40b4b87d6589c41e08d3cc1c2b539c4c2922982
1u1s4 <99617682+1u1s4@users.noreply.github.com>
2022-07-29 11:07:38 -0600

    bug
    no tomaba los dias del ultimo mes del reporte

--- petroleo.py
+++ petroleo.py
@@ -1,8 +1,9 @@
 from fredapi import Fred
 from datetime import datetime, timedelta
-from datetimeLAAR import year_ago, month_after, date_mini, mes_by_ordinal
+from datetimeLAAR import year_ago, anio_mes, mes_by_ordinal
+import descriptor
 fred = Fred(api_key='734b605521e7734edc09f38e977fe238')
-FECHA_REPORTE = "2015-11-22"#datetime.today().strftime("%Y-%m-%d")
+FECHA_REPORTE = datetime.today().strftime("%Y-%m-%d")
 FECHA_ANTERIOR = year_ago(FECHA_REPORTE)
 # carga de datos
 data = fred.get_series('DCOILWTICO', observation_start=FECHA_ANTERIOR, observation_end=FECHA_REPORTE)
@@ -14,8 +15,8 @@ data_mean = []
 while fecha_i != FECHA_REPORTE:
     fecha_i = datetime.strptime(fecha_i, "%Y-%m-%d") + timedelta(days=1)
     fecha_i = fecha_i.strftime("%Y-%m-%d")
-    mes_actual_i = "-".join((fecha_i.split("-")[0], mes_by_ordinal(fecha_i.split("-")[1])))
-    if mes_actual_i != mes_actual:
+    mes_actual_i = anio_mes(fecha_i)
+    if mes_actual_i != mes_actual or fecha_i == FECHA_REPORTE:
         mean = sum(datos_mes)/len(datos_mes)
         data_mean.append((mes_actual, f"{mean:.2f}"))
         datos_mes = []
@@ -27,3 +28,4 @@ while fecha_i != FECHA_REPORTE:
         None
 for i in data_mean:
     print(i)
+print(descriptor.petroleo(data_mean))
