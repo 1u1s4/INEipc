e00440cef2678c6418dbdb0dd36354faf153bd9a
laar <3553751921101@ingenieria.usac.edu.gt>
2022-07-29 20:21:46 -0600

    arreglo formula variacion

--- descriptor.py
+++ descriptor.py
@@ -1,5 +1,8 @@
+from numpy import var
 from datetimeLAAR import mes_anio_by_abreviacion
 
+def variacion(dato: float, dato_antes: float) -> float:
+    return ((dato - dato_antes) / dato_antes) * 100
 # petroleo
 """
 ejemplo de datos
@@ -15,22 +18,22 @@ def petroleo(datos: list[tuple[str]]) -> str:
     FECHA_2 = mes_anio_by_abreviacion(datos[0][0])
     FECHA_3 = mes_anio_by_abreviacion(datos[-2][0])
     PRECIO = datos[-1][1]
-    DIFERANCIA_1 = float(datos[-1][1]) - float(datos[0][1])
-    VARIACION_1 = (DIFERANCIA_1 / float(datos[-1][1])) * 100
+    DIFERANCIA_1 = datos[-1][1] - datos[0][1]
+    VARIACION_1 = variacion(datos[-1][1], datos[0][1])
     if DIFERANCIA_1 > 0:
         SIGNO_1 = ""
     else:
         DIFERANCIA_1 = DIFERANCIA_1 * -1
         SIGNO_1 = "-"
-    DIFERANCIA_2 = float(datos[-1][1]) - float(datos[-2][1])
-    VARIACION_2 = (DIFERANCIA_2 / float(datos[-1][1])) * 100
+    DIFERANCIA_2 = datos[-1][1] - datos[-2][1]
+    VARIACION_2 = variacion(datos[-1][1], datos[-2][1])
     if DIFERANCIA_2 > 0:
         SIGNO_2 = ""
     else:
         DIFERANCIA_2 = DIFERANCIA_2 * -1
         SIGNO_2 = "-"
-    PLANTILLA = f"""El precio internacional del petróleo a registró en {FECHA_1}
-                un precio medio de US${PRECIO} por barril, lo que representa
+    PLANTILLA = f"""El precio internacional del petróleo registró en {FECHA_1}
+                un precio medio de US${PRECIO:.2f} por barril, lo que representa
                 una variación de {VARIACION_1:.2f}% ({SIGNO_1}US${DIFERANCIA_1:.2f})
                 respecto a {FECHA_2} y de {VARIACION_2:.2f}% ({SIGNO_2}US${DIFERANCIA_2:.2f}) respecto a {FECHA_3}.
                 """
@@ -53,8 +56,8 @@ def cambio_del_quetzal(datos: list[tuple[str]]) -> str:
     FECHA_2 = mes_anio_by_abreviacion(datos[0][0])
     FECHA_3 = mes_anio_by_abreviacion(datos[-2][0])
     PRECIO = datos[-1][1]
-    VARIACION_1 = ((float(datos[-1][1]) - float(datos[0][1])) / float(datos[-1][1])) * 100
-    VARIACION_2 = ((float(datos[-1][1]) - float(datos[-2][1])) / float(datos[-1][1])) * 100
+    VARIACION_1 = variacion(datos[-1][1], datos[0][1])
+    VARIACION_2 = variacion(datos[-1][1], datos[-2][1])
     PLANTILLA = f"""El tipo de cambio de referencia del
                 quetzal respecto al dólar de los Estados
                 Unidos de América, registró en {FECHA_1} 
