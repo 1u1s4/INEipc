c06d2a5af28d54fe14b808b9491bc27446312c87
laar <3553751921101@ingenieria.usac.edu.gt>
2022-07-29 20:27:42 -0600

    update

--- setup.py
+++ setup.py
@@ -1,16 +1,17 @@
 from setuptools import setup
 setup(
-    name='app-name',
-    version='1.0',
-    author='Bejamin Frakline',
-    description='A brief synopsis of the project',
-    long_description='A much longer explanation of the project and helpful resources',
-    url='https://github.com/BenjaminFranline',
+    name='automatizacion_capitulo_1_IPC_INE',
+    version='0.1',
+    author='Luis Alfredo Alvarado Rodríguez',
+    description='Automatizacion de extraccion de datos para el capitulo 1 del IPC.',
+    long_description='',
+    url='https://github.com/1u1s4/INE_IPC',
     keywords='development, setup, setuptools',
-    python_requires='>=3.7',
+    python_requires='>=3.9',
     install_requires=[
         'fredapi',
         'xlrd',
-        'prettytable'
+        'prettytable',
+        'rpy2'
     ]
 )
