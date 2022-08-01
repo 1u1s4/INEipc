4a0059d308ac6a82a21a702c8ca01c85f795fbe5
1u1s4 <99617682+1u1s4@users.noreply.github.com>
2022-08-01 11:03:30 -0600

    pruebas

--- orquestador_r.py
+++ orquestador_r.py
@@ -2,6 +2,12 @@
 import os
 os.environ["R_HOME"] = r"C:\Program Files\R\R-4.2.1" # change as needed
 import rpy2.robjects.packages as rpackages
-
-devtools = rpackages.importr('devtools')
+import rpy2.robjects as robjects
+#devtools = rpackages.importr('devtools')
 #devtools.install_github("1u1s4/funcionesINE")
+funcionesINE = rpackages.importr('funcionesINE')
+ruta = 'C:/Users/laalvarado/Documents/pruebas/'
+funcionesINE.escribirCSV(
+    funcionesINE.leerLibroNormal(f'{ruta}/Libros/pobrezaCocinado.xlsx'),
+    ruta=f"{ruta}CSVENCOVI/"
+    )
