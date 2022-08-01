fd1a96907c99884c408edcae9a1e9aaefc054691
1u1s4 <99617682+1u1s4@users.noreply.github.com>
2022-08-01 09:30:01 -0600

    para ignorar datos faltantes

--- ipc_usa.py
+++ ipc_usa.py
@@ -2,19 +2,22 @@ from fredapi import Fred
 from funcionesjo import year_ago, month_after, mes_by_ordinal, hoy
 
 fred = Fred(api_key='734b605521e7734edc09f38e977fe238')
-FECHA_REPORTE = "2022-06-01"#hoy("%Y-%m-%d")
+FECHA_REPORTE = hoy("%Y-%m-01")#"2015-11-01"
 FECHA_ANTERIOR = year_ago(FECHA_REPORTE)
-FECHA_ANTERIOR_ANTERIOR = year_ago(FECHA_ANTERIOR)
+FECHA_ANTERIOR_ANTERIOR = str(int(FECHA_REPORTE.split("-")[0]) - 2) + "-01-01"
 data = fred.get_series('CPIAUCSL', observation_start=FECHA_ANTERIOR_ANTERIOR, observation_end=FECHA_REPORTE)
 fecha_i = FECHA_ANTERIOR
 datos_variacion_interanual = []
 for i in range(13):
-    precio = data.dropna().loc[fecha_i]
-    precio_anterior = data.dropna().loc[year_ago(fecha_i)]
-    mes_actual_i = "-".join((fecha_i.split("-")[0], mes_by_ordinal(fecha_i.split("-")[1])))
-    variacion = ((precio / precio_anterior) - 1) * 100
-    datos_variacion_interanual.append((mes_actual_i, f"{variacion:.2f}"))
-    fecha_i = month_after(fecha_i)
+    try:
+        precio = data.dropna().loc[fecha_i]
+        precio_anterior = data.dropna().loc[year_ago(fecha_i)]
+        mes_actual_i = "-".join((fecha_i.split("-")[0], mes_by_ordinal(fecha_i.split("-")[1])))
+        variacion = ((precio / precio_anterior) - 1) * 100
+        datos_variacion_interanual.append((mes_actual_i, variacion))
+        fecha_i = month_after(fecha_i)
+    except:
+        pass
 
 for i in datos_variacion_interanual:
     print(i)
