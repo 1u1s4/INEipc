ff859757e505e0978bfc2e9650ad71cca229a214
1u1s4 <99617682+1u1s4@users.noreply.github.com>
2022-08-04 10:56:42 -0600

    cambiar forma de importar funcionesjo

--- datosipc.py
+++ datosipc.py
@@ -3,7 +3,7 @@ import requests
 import json
 import xlrd
 from fredapi import Fred
-from funcionesjo import year_ago, anio_mes, mes_by_ordinal, hoy, day_after, month_after, date_mini, ultimo_dia_del_mes
+import funcionesjo as Jo
 import descriptoripc
 
 class datosIPC:
@@ -15,11 +15,11 @@ class datosIPC:
         SERIES_ID = 'DCOILWTICO'
         fred = Fred(api_key=API_KEY)
         if len(fecha_hasta) == 0:
-            FECHA_REPORTE = hoy(self._FORMATO)
+            FECHA_REPORTE = Jo.hoy(self._FORMATO)
         else:
             FECHA_REPORTE = fecha_hasta
         if len(fecha_desde) == 0:
-            FECHA_ANTERIOR = year_ago(fecha=FECHA_REPORTE)
+            FECHA_ANTERIOR = Jo.year_ago(fecha=FECHA_REPORTE)
         else:
             FECHA_ANTERIOR = fecha_desde
         # carga de datos
@@ -30,12 +30,12 @@ class datosIPC:
         )
         # extraccion de los datos en el rango de fechas
         fecha_i = FECHA_ANTERIOR
-        mes_actual = "-".join((fecha_i.split("-")[0], mes_by_ordinal(fecha_i.split("-")[1])))
+        mes_actual = "-".join((fecha_i.split("-")[0], Jo.mes_by_ordinal(fecha_i.split("-")[1])))
         datos_mes = []
         data_mean = []
         while fecha_i != FECHA_REPORTE:
-            fecha_i = day_after(fecha_i)
-            mes_actual_i = anio_mes(fecha_i)
+            fecha_i = Jo.day_after(fecha_i)
+            mes_actual_i = Jo.anio_mes(fecha_i)
             if mes_actual_i != mes_actual or fecha_i == FECHA_REPORTE:
                 mean = sum(datos_mes) / len(datos_mes)
                 data_mean.append((mes_actual, mean))
@@ -51,11 +51,11 @@ class datosIPC:
     def cambio_quetzal(self, fecha_hasta="", fecha_desde="") -> list[tuple]:
         FORMATO = "%d/%m/%Y"
         if len(fecha_hasta) == 0:
-            FECHA_REPORTE = hoy(FORMATO)
+            FECHA_REPORTE = Jo.hoy(FORMATO)
         else:
-            FECHA_REPORTE = ultimo_dia_del_mes(fecha=fecha_hasta, formato=FORMATO)
+            FECHA_REPORTE = Jo.ultimo_dia_del_mes(fecha=fecha_hasta, formato=FORMATO)
         if len(fecha_desde) == 0:
-            FECHA_ANTERIOR = year_ago(fecha=FECHA_REPORTE, formato=FORMATO, inicio_de_mes=True)
+            FECHA_ANTERIOR = Jo.year_ago(fecha=FECHA_REPORTE, formato=FORMATO, inicio_de_mes=True)
         else:
             FECHA_ANTERIOR = fecha_desde
         # SOAP request URL
@@ -83,14 +83,14 @@ class datosIPC:
         root = tree.getroot()
         # analisis de los datos
         fecha_i = FECHA_ANTERIOR
-        mes_actual = "-".join((fecha_i.split("/")[2], mes_by_ordinal(fecha_i.split("/")[1])))
+        mes_actual = "-".join((fecha_i.split("/")[2], Jo.mes_by_ordinal(fecha_i.split("/")[1])))
         datos_mes = []
         data_mean = []
         i = 0
         for cambio in root[0][0][0][0]:
             i += 1
             fecha_i = cambio[1].text
-            mes_actual_i = "-".join((fecha_i.split("/")[2], mes_by_ordinal(fecha_i.split("/")[1])))
+            mes_actual_i = "-".join((fecha_i.split("/")[2], Jo.mes_by_ordinal(fecha_i.split("/")[1])))
             # la ultima comparacion es para calcular el promedio del ultimo mes
             if mes_actual_i != mes_actual or i == len(root[0][0][0][0]):
                 data_mean.append((mes_actual, sum(datos_mes)/len(datos_mes)))
@@ -106,11 +106,11 @@ class datosIPC:
     def tasa_interes(self, fecha_hasta="", fecha_desde="") -> list[tuple]:
         FORMATO = "%Y-%m"
         if len(fecha_hasta) == 0:
-            FECHA_REPORTE = hoy(FORMATO)
+            FECHA_REPORTE = Jo.hoy(FORMATO)
         else:
             FECHA_REPORTE = fecha_hasta
         if len(fecha_desde) == 0:
-            FECHA_ANTERIOR = year_ago(fecha=FECHA_REPORTE, formato=FORMATO)
+            FECHA_ANTERIOR = Jo.year_ago(fecha=FECHA_REPORTE, formato=FORMATO)
         else:
             FECHA_ANTERIOR = fecha_desde
         # descarga de datos
@@ -126,13 +126,13 @@ class datosIPC:
         data = []
         COL = int(FECHA_ANTERIOR.split("-")[0]) - 1994
         for i in range(int(FECHA_ANTERIOR.split("-")[1]) + 4, 12 + 5):
-            marca_temp = FECHA_ANTERIOR.split("-")[0] + "-" + mes_by_ordinal(str(i - 4).rjust(2, "0"))
+            marca_temp = FECHA_ANTERIOR.split("-")[0] + "-" + Jo.mes_by_ordinal(str(i - 4).rjust(2, "0"))
             interes = sh.cell_value(rowx=i, colx=COL)
             if interes != "":
                 data.append((marca_temp, 100*interes))
         COL = int(FECHA_REPORTE.split("-")[0]) - 1994
         for i in range(5, int(FECHA_REPORTE.split("-")[1]) + 4 + 1):
-            marca_temp = FECHA_REPORTE.split("-")[0] + "-" + mes_by_ordinal(str(i - 4).rjust(2, "0"))
+            marca_temp = FECHA_REPORTE.split("-")[0] + "-" + Jo.mes_by_ordinal(str(i - 4).rjust(2, "0"))
             interes = sh.cell_value(rowx=i, colx=COL)
             if interes != "":
                 data.append((marca_temp, 100*interes))
@@ -140,14 +140,14 @@ class datosIPC:
 
     def ipc_usa(self, fecha_hasta="", fecha_desde="") -> list[tuple]:
         if len(fecha_hasta) == 0:
-            FECHA_REPORTE = hoy(self._FORMATO, inicio_de_mes=True)
+            FECHA_REPORTE = Jo.hoy(self._FORMATO, inicio_de_mes=True)
         else:
             FECHA_REPORTE = fecha_hasta
         if len(fecha_desde) == 0:
-            FECHA_ANTERIOR = year_ago(fecha=FECHA_REPORTE)
+            FECHA_ANTERIOR = Jo.year_ago(fecha=FECHA_REPORTE)
         else:
             FECHA_ANTERIOR = fecha_desde
-        FECHA_ANTERIOR_ANTERIOR = year_ago(fecha=FECHA_ANTERIOR, inicio_de_anio=True)
+        FECHA_ANTERIOR_ANTERIOR = Jo.year_ago(fecha=FECHA_ANTERIOR, inicio_de_anio=True)
         API_KEY = '734b605521e7734edc09f38e977fe238'
         fred = Fred(api_key=API_KEY)
         data = fred.get_series('CPIAUCSL', observation_start=FECHA_ANTERIOR_ANTERIOR, observation_end=FECHA_REPORTE)
@@ -156,22 +156,22 @@ class datosIPC:
         for i in range(13):
             try:
                 precio = data.dropna().loc[fecha_i]
-                precio_anterior = data.dropna().loc[year_ago(fecha_i)]
-                mes_actual_i = "-".join((fecha_i.split("-")[0], mes_by_ordinal(fecha_i.split("-")[1])))
+                precio_anterior = data.dropna().loc[Jo.year_ago(fecha_i)]
+                mes_actual_i = "-".join((fecha_i.split("-")[0], Jo.mes_by_ordinal(fecha_i.split("-")[1])))
                 variacion = ((precio / precio_anterior) - 1) * 100
                 datos_variacion_interanual.append((mes_actual_i, variacion))
-                fecha_i = month_after(fecha_i)
+                fecha_i = Jo.month_after(fecha_i)
             except:
                 pass
         return (datos_variacion_interanual, descriptoripc.ipc_usa(datos_variacion_interanual))
 
     def ipc_mex(self, fecha_hasta="", fecha_desde="") -> list[tuple]:
         if len(fecha_hasta) == 0:
-            FECHA_REPORTE = hoy(self._FORMATO)
+            FECHA_REPORTE = Jo.hoy(self._FORMATO)
         else:
             FECHA_REPORTE = fecha_hasta
         if len(fecha_desde) == 0:
-            FECHA_ANTERIOR = year_ago(fecha=FECHA_REPORTE)
+            FECHA_ANTERIOR = Jo.year_ago(fecha=FECHA_REPORTE)
         else:
             FECHA_ANTERIOR = fecha_desde
         API_KEY = "515963d6-1153-e348-8394-a81acec0d6da"
@@ -190,12 +190,12 @@ class datosIPC:
         datos_variacion_interanual = []
         for i in range(13):
             try:
-                precio = data[date_mini(fecha_i)]
-                precio_anterior = data[date_mini(year_ago(fecha_i))]
-                mes_actual_i = "-".join((fecha_i.split("-")[0], mes_by_ordinal(fecha_i.split("-")[1])))
+                precio = data[Jo.date_mini(fecha_i)]
+                precio_anterior = data[Jo.date_mini(Jo.year_ago(fecha_i))]
+                mes_actual_i = "-".join((fecha_i.split("-")[0], Jo.mes_by_ordinal(fecha_i.split("-")[1])))
                 variacion = ((precio / precio_anterior) - 1) * 100
                 datos_variacion_interanual.append((mes_actual_i, variacion))
-                fecha_i = month_after(fecha_i)
+                fecha_i = Jo.month_after(fecha_i)
             except:
                 pass
         return (datos_variacion_interanual, descriptoripc.ipc_mex(datos_variacion_interanual))
