502a00c4982c0cf1a5d1d0721ddc0498503737dd
1u1s4 <99617682+1u1s4@users.noreply.github.com>
2022-08-23 08:39:12 -0600

    Revert "package"
    This reverts commit 2f56ee0721e1ea1e37c57472d3228b58f0f2bc43.

--- setup.py
+++ setup.py
@@ -1,17 +1,16 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 setup(
-    name='datosIPC',
-    version='1.0',
+    name='automatizacion_capitulo_1_IPC_INE',
+    version='0.1',
     author='Luis Alfredo Alvarado Rodríguez',
     description='Automatizacion de extraccion de datos para el capitulo 1 del IPC.',
     long_description='',
     url='https://github.com/1u1s4/INE_IPC',
     keywords='development, setup, setuptools',
     python_requires='>=3.9',
-    packages=find_packages(),
     install_requires=[
         'fredapi',
-        'xlrd==1.2.0',
+        'xlrd=1.2.0',
         'prettytable',
         'rpy2',
         'xlsxwriter'
