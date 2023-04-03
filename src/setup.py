c0310eaffaeabfdba56a930b670408c0a95b27d1
Luis Alvarado <99617682+1u1s4@users.noreply.github.com>
2023-04-03 10:29:52 -0600

    Update setup.py

--- src/setup.py
+++ src/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name='automatizacion_IPC_INE',
-    version='1.0.9',
+    version='1.0.14',
     author='Luis Alfredo Alvarado Rodríguez',
     description='Automatizacion de extraccion de datos para el capitulo 1 del IPC.',
     long_description='',
