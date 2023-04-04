e874b654709abaf865d4343511aa767187d40c08
1u1s4 <3553751921101@ingenieria.usac.edu.gt>
2023-04-04 02:23:02 -0600

    Update setup.py

--- src/setup.py
+++ src/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name='automatizacion_IPC_INE',
-    version='1.0.24',
+    version='1.0.28',
     author='Luis Alfredo Alvarado Rodríguez',
     description='ETL para el informe mensual de IPC.',
     long_description='',
