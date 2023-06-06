ae8ad7eb515164823967625a719de733c014d6ce
1u1s4 <3553751921101@ingenieria.usac.edu.gt>
2023-06-06 15:40:34 -0600

    fuentes no precios

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name='ineipc',
-    version='1.0.11',
+    version='1.0.12',
     author='Luis Alfredo Alvarado Rodríguez',
     description='ETL para el informe mensual de IPC.',
     long_description='',
