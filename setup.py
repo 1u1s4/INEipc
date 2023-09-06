6f8fd88ff5782dba84e42c533072c89e0bfbd68e
Luis Alvarado <99617682+1u1s4@users.noreply.github.com>
2023-09-06 09:15:27 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name='INEipc',
-    version='1.3.7',
+    version='1.4',
     author='Luis Alfredo Alvarado Rodríguez',
     description='ETL para el informe mensual de IPC.',
     long_description='',
