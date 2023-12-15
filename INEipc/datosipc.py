f7715a341af6d3ad75557bf1efbee992c73c7ae3
ShannonNCM <61364989+ShannonNCM@users.noreply.github.com>
2023-12-15 15:21:18 -0600

    Update datosipc.py

--- INEipc/datosipc.py
+++ INEipc/datosipc.py
@@ -63,7 +63,7 @@ class DatosIPC:
             r = requests.get(DATA_URL, allow_redirects=True)
             f.write(r.content)
             f.close()
-        df = pd.read_excel('FFPI.csv', header=2, usecols=[0, 1])
+        df = pd.read_excel('FFPI.xls', header=2, usecols=[0, 1])
         df['Date'] = df['Date'].astype('str')
         data = []
         for i in range(13):
